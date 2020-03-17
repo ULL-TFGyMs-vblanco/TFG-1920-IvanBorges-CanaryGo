5a2df3fd96dcfa44d5d2523a41a50df43c041e68
Ivan Josue Borges Gonzalez <alu0100880755@ull.edu.es>
2020-03-17 16:35:43 +0000

    Index inicial

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,33 +17,36 @@
           />
         </q-card-actions>
 
-        <q-slide-transition>
-          <div v-show="expanded">
-            <q-separator />
-            <q-card-section class="text-subitle2">
-              <q-img class="my-card" src="https://cdn.quasar.dev/img/parallax2.jpg" />
-              {{ info1}}
-            </q-card-section>
+        <template v-slot:content="scope">
+          <div
+            class="absolute column items-center"
+            :style="{
+            opacity: 0.45 + (1 - scope.percentScrolled) * 0.55,
+            top: (scope.percentScrolled * 60) + '%',
+            left: 0,
+            right: 0
+          }"
+          >
+            <img
+              src="../assets/images/CanaryGo/Canary_Go_Icon.png"
+              style="width: 150px; height: 150px"
+            />
+            <div class="text-h3 text-bold text-black text-center">Encuentra los eventos que tanto buscas</div>
+            <div class="text-h6 text-grey-3 text-center">Mas info</div>
           </div>
-        </q-slide-transition>
+        </template>
       </q-parallax>
 
-      <div class="info1">
-        <q-parallax :height="600" :speed="0.8" src="../assets/images/index/beach-1853939.jpg">
-          <h1 class="text-white text-center">Encuentra los eventos que tanto buscas</h1>
-        </q-parallax>
-      </div>
-
       <q-parallax :height="600" :speed="0.5" src="../assets/images/index/paddle-839814.jpg">
-        <h1 class="text-white text-center">Organiza el calendario a tu manera</h1>
+        <h1 class="text-h3 text-bold text-white text-center">Organiza el calendario a tu manera</h1>
       </q-parallax>
 
       <q-parallax :height="600" :speed="0.8" src="../assets/images/index/concert-768722.jpg">
-        <h1 class="text-white text-center">Conoce gente nueva dentro de la comunidad</h1>
+        <h1 class="text-h3 text-bold text-white text-center">Conoce gente nueva dentro de la comunidad</h1>
       </q-parallax>
 
       <q-parallax :height="600" :speed="0.6" src="../assets/images/index/yoga-2176668.jpg">
-        <h1 class="text-white text-center">Basic</h1>
+        <h1 class="text-h3 text-bold text-white text-center">Mejora en la gestión de viajes</h1>
       </q-parallax>
     </div>
   </div>
@@ -73,5 +76,4 @@ export default {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 <style>
-
 </style>
