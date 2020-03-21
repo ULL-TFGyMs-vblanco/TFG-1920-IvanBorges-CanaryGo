98bc067b01ea1adc7637176facb16d28b55bffac
Ivan Josue Borges Gonzalez <alu0100880755@ull.edu.es>
2020-03-21 05:04:25 +0000

    Primera version registro

--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -1,16 +1,198 @@
 <template>
-  <div class="q-pa-md"></div>
+  <div class="row text-center">
+    <div class="q-pa-md" id="formulario" style="width: 100%;">
+      <img
+        src="../assets/images/CanaryGo/Canary_Go_Icon.png"
+        style="width: 100px; height: 100px; border-radius: 20%;"
+      />
+      <br />
+      <br />
+      <br />
+      <form @submit.prevent.stop="onSubmit" @reset.prevent.stop="onReset" class="q-gutter-md">
+        <q-input
+          ref="nombre"
+          filled
+          v-model="nombre"
+          label="Nombre *"
+          hint="Escribe tu nombre"
+          type="name"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Debes introducir un nombre']"
+        />
+
+        <q-input
+          ref="usuario"
+          filled
+          v-model="usuario"
+          label="Usuario *"
+          hint="Elige tu usuario"
+          type="user"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Debes introducir un usuario']"
+        />
+
+        <q-input
+          ref="fecha"
+          filled
+          v-model="fecha"
+          label="Fecha de nacimiento *"
+          hint="Indica tu fecha de nacimiento"
+          type="date"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Debes introducir una fecha']"
+        />
+
+        <q-input
+          ref="email"
+          filled
+          v-model="email"
+          label="Email *"
+          hint="Escribe tu email"
+          type="email"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Debes introducir un email']"
+        />
+
+        <q-input
+          ref="contrasena"
+          filled
+          v-model="contrasena"
+          :type="isPwd ? 'password' : 'text'"
+          label="Contraseña *"
+          hint="Escribe tu contraseña"
+          lazy-rules
+          :rules="[
+          val => val !== null && val !== '' || 'Debes introducir la contraseña',
+        ]"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+
+        <q-input
+          ref="contrasena2"
+          filled
+          v-model="contrasena2"
+          :type="isPwd ? 'password' : 'text'"
+          label="Contraseña *"
+          hint="Repite la contraseña"
+          lazy-rules
+          :rules="[
+          val => val !== null && val !== '' || 'Repite la contraseña',
+        ]"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+
+        <div class="text-center">
+          <q-checkbox
+            name="sesion"
+            v-model="sesion"
+            label="Acepto los términos y condiciones de privacidad"
+          />
+          <br />
+
+          <q-item clickable v-ripple to="/restore">
+            <q-item-section>
+              <q-item-label style="color: #ec9718">He olvidado mi contraseña</q-item-label>
+            </q-item-section>
+          </q-item>
+        </div>
+        <div>
+          <q-btn label="Registrarse" type="submit" color="primary" />
+          <q-btn label="Limpiar" type="reset" color="primary" flat class="q-ml-sm" />
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  email: 'Signup',
   data () {
     return {
+      nombre: null,
+      usuario: null,
+      email: null,
+      fecha: null,
+      contrasena: null,
+      isPwd: true,
+      sesion: false
+    }
+  },
+
+  methods: {
+    onSubmit () {
+      this.$refs.email.validate()
+      this.$refs.contrasena.validate()
+
+      if (this.$refs.email.hasError || this.$refs.contrasena.hasError) {
+        this.formHasError = true
+      } else if (this.sesion !== true) {
+        this.$q.notify({
+          color: 'negative',
+          message: 'Debes aceptar los términos y condiciones',
+          position: 'bottom',
+          timeout: 2000,
+          progress: true
+        })
+      } else {
+        this.$q.notify({
+          icon: 'done',
+          color: 'positive',
+          message: 'Registro correcto',
+          position: 'bottom',
+          timeout: 1000,
+          progress: true
+        })
+      }
+    },
+
+    onReset () {
+      this.nombre = null
+      this.usuario = null
+      this.fecha = null
+      this.email = null
+      this.contrasena = null
+      this.contrasena2 = null
+
+      this.$refs.nombre.resetValidation()
+      this.$refs.usuario.resetValidation()
+      this.$refs.fecha.resetValidation()
+      this.$refs.email.resetValidation()
+      this.$refs.contrasena.resetValidation()
+      this.$refs.contrasena2.resetValidation()
     }
   }
 }
 </script>
 
 <style>
+.negrita {
+  font-weight: bold;
+  font-size: 120%;
+}
 
+#formulario {
+  max-width: 100%;
+  /* padding-right: 20%;
+  padding-left: 20%; */
+
+  padding-right: 8%;
+  padding-left: 8%;
+  padding-top: 10%;
+  padding-bottom: 10%;
+}
 </style>
