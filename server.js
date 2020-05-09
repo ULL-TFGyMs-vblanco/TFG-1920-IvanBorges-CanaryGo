bd24b7d2ae4b374eda816baee43d2700ed88b3b8
Ivan Josue Borges Gonzalez <alu0100880755@ull.edu.es>
2020-05-09 23:39:51 +0100

    fix v3

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use('/', router)
 app.use(express.static(__dirname + '/dist/instatech'))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/instatech/index.html'))
+  res.sendFile(path.join(__dirname + '/dist/spa/index.html'))
 })
 
 // Start the app by listening on the default Heroku port
