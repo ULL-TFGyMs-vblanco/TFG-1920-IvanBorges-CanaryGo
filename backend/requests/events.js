0a4a2544c9a30aa89befa5fb88e3d028781b3024
Ivan Josue Borges Gonzalez <alu0100880755@ull.edu.es>
2020-05-11 15:25:36 +0100

    Configurando server

--- backend/requests/events.js
+++ backend/requests/events.js
@@ -7,108 +7,110 @@ module.exports = function (app) {
   app.get('/eventos', function (req, res) {
     console.log('HTTP Obtener Evento')
     // res.send('HTTP Obtener Evento')
-
-    // Consultas
-    var eventos = firebaseDb.collection('eventos')
-    var destacados = eventos.orderBy('votos', 'desc')
-    var novedades = eventos.orderBy('fecha_creacion', 'desc')
-    var zona = eventos.orderBy('fecha_creacion', 'desc')
-    // var isla = eventos.where('isla', '==', 'Tenerife')
-
-    // Gestión consultas
-    var consulta
-    if (req.body.tab === 'tab1') {
-      consulta = destacados
-    } else if (req.body.tab === 'tab2') {
-      consulta = novedades
-    } else if (req.body.tab === 'tab3') {
-      consulta = zona
-    }
-
-    // Requerimos filtro por isla si es necesario
-    if (req.body.isla !== '') {
-      consulta = consulta.where('isla', '==', req.body.isla)
-    }
-
-    /// ///////////
-
-    // Leer bbdd
-    const datos_evento = []
-
-    consulta.get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-      // Leemos los datos
-        const evento = {
-          nombre_evento: doc.data().nombre_evento,
-          localizacion: doc.data().localizacion,
-          precio: doc.data().precio,
-          fecha_inicio: doc.data().fecha_inicio,
-          votos: doc.data().votos,
-          comentarios: doc.data().comentarios,
-          usuario: doc.data().usuario,
-          isla: doc.data().isla,
-          id: doc.id,
-          foto_usuario: doc.data().foto_usuario
-        }
-
-        const storageRef = firebaseStg.ref('eventos/' + doc.id)
-        var fotoRef = storageRef.child('foto')
-
-        // Obtener foto
-        fotoRef.getDownloadURL().then(function (url) {
-          evento.foto = url
-        }).then(function (docRef) {
-
-        })
-        datos_evento.push(evento)
-      })
-    })
-      .then(function (snapshot) {
-        console.log('Eventos leidos')
-        res.send(datos_evento)
-      })
-      .catch(function (error) {
-        console.error('Error leyendo eventos', error)
-        res.send('Error al leer los eventos')
-      })
   })
 
   // Put
   app.put('/eventos', function (req, res) {
     console.log('HTTP Crear Evento')
 
-    // Insertar db
-    firebaseDb.collection('prueba').add({
-      Nombre_evento: req.body.nombre_evento,
-      Localizacion: req.body.localizacion,
-      Fecha_inicio: req.body.fecha_inicio,
-      Fecha_fin: req.body.fecha_fin,
-      Precio: req.body.precio,
-      Descuento: req.body.descuento,
-      Descripcion: req.body.descripcion,
-      Votos: 0,
-      Comentarios: 0,
-      Usuario: req.body.usuario,
-      Isla: req.body.isla,
-      Fecha_creacion: new Date().getDay() + '/' + new Date().getMonth() + '/' + new Date().getFullYear() + ',' + new Date().getHours() + ':' + new Date().getMinutes() + new Date().getMilliseconds(),
-      Foto_usuario: firebaseAuth.currentUser.photoURL
-    })
-      .then(function (docRef) {
-      // Subir imagenes
-        console.log('Eve')
-        const storageRef = firebaseStg.ref('prueba/' + docRef.id)
-        const thisRef = storageRef.child('foto')
+    if (req.body.tipo === 'Consultar') {
+      // Consultas
+      var eventos = firebaseDb.collection('eventos')
+      var destacados = eventos.orderBy('votos', 'desc')
+      var novedades = eventos.orderBy('fecha_creacion', 'desc')
+      var zona = eventos.orderBy('fecha_creacion', 'desc')
+      // var isla = eventos.where('isla', '==', 'Tenerife')
 
-      // thisRef.put(req.body.file)
-      //   .then(function (snapshot) {
-      //     console.log('Archivo subido')
-      //     res.send('Evento añadido')
-      //   })
+      // Gestión consultas
+      var consulta
+      if (req.body.tab === 'tab1') {
+        consulta = destacados
+      } else if (req.body.tab === 'tab2') {
+        consulta = novedades
+      } else if (req.body.tab === 'tab3') {
+        consulta = zona
+      }
+
+      // Requerimos filtro por isla si es necesario
+      if (req.body.isla !== '') {
+        consulta = consulta.where('isla', '==', req.body.isla)
+      }
+
+      /// ///////////
+
+      // Leer bbdd
+      const datos_evento = []
+
+      consulta.get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          // Leemos los datos
+          const evento = {
+            nombre_evento: doc.data().nombre_evento,
+            localizacion: doc.data().localizacion,
+            precio: doc.data().precio,
+            fecha_inicio: doc.data().fecha_inicio,
+            votos: doc.data().votos,
+            comentarios: doc.data().comentarios,
+            usuario: doc.data().usuario,
+            isla: doc.data().isla,
+            id: doc.id,
+            foto_usuario: doc.data().foto_usuario
+          }
+
+          const storageRef = firebaseStg.ref('eventos/' + doc.id)
+          var fotoRef = storageRef.child('foto')
+
+          // Obtener foto
+          fotoRef.getDownloadURL().then(function (url) {
+            evento.foto = url
+          }).then(function (docRef) {
+
+          })
+          datos_evento.push(evento)
+        })
       })
-      .catch(function (error) {
-        console.error('Error añadiendo evento ', error)
-        res.send('Error al crear Evento')
+        .then(function (snapshot) {
+          console.log('Eventos leidos')
+          res.send(datos_evento)
+        })
+        .catch(function (error) {
+          console.error('Error leyendo eventos', error)
+          res.send('Error al leer los eventos')
+        })
+    } else if (req.body.tipo === 'Crear') {
+      // Insertar db
+      firebaseDb.collection('prueba').add({
+        Nombre_evento: req.body.nombre_evento,
+        Localizacion: req.body.localizacion,
+        Fecha_inicio: req.body.fecha_inicio,
+        Fecha_fin: req.body.fecha_fin,
+        Precio: req.body.precio,
+        Descuento: req.body.descuento,
+        Descripcion: req.body.descripcion,
+        Votos: 0,
+        Comentarios: 0,
+        Usuario: req.body.usuario,
+        Isla: req.body.isla,
+        Fecha_creacion: new Date().getDay() + '/' + new Date().getMonth() + '/' + new Date().getFullYear() + ',' + new Date().getHours() + ':' + new Date().getMinutes() + new Date().getMilliseconds(),
+        Foto_usuario: firebaseAuth.currentUser.photoURL
       })
+        .then(function (docRef) {
+          // Subir imagenes
+          console.log('Eve')
+          const storageRef = firebaseStg.ref('prueba/' + docRef.id)
+          const thisRef = storageRef.child('foto')
+
+          // thisRef.put(req.body.file)
+          //   .then(function (snapshot) {
+          //     console.log('Archivo subido')
+          //     res.send('Evento añadido')
+          //   })
+        })
+        .catch(function (error) {
+          console.error('Error añadiendo evento ', error)
+          res.send('Error al crear Evento')
+        })
+    }
   })
 
   // Update
--- src/pages/Eventos.vue
+++ src/pages/Eventos.vue
@@ -2,9 +2,24 @@
   <q-page padding>
     <!-- <Toolbar></Toolbar> -->
     <Usuario :key="$i18n.locale"></Usuario>
-    <Filtro class="filtro" @clicked="DataChild" />
-    <Evento class="evento" v-for="dato in datos_evento" :key="dato.nombre_evento" v-bind="dato"></Evento>
-    <q-btn class="boton_add" round color="primary" icon="add" size="150%" to="/new" />
+    <Filtro
+      class="filtro"
+      @clicked="DataChild"
+    />
+    <Evento
+      class="evento"
+      v-for="dato in datos_evento"
+      :key="dato.nombre_evento"
+      v-bind="dato"
+    ></Evento>
+    <q-btn
+      class="boton_add"
+      round
+      color="primary"
+      icon="add"
+      size="150%"
+      to="/new"
+    />
   </q-page>
 </template>
 
@@ -13,7 +28,7 @@ import Evento from 'components/Eventos/Evento'
 import Usuario from 'components/Eventos/Usuario'
 import Filtro from 'components/Eventos/Filtro'
 // import Toolbar from 'components/Toolbar'
-import { firebaseDb, firebaseStg } from 'boot/firebase'
+import axios from 'axios'
 
 export default {
   name: 'Eventos',
@@ -30,67 +45,22 @@ export default {
     }
   },
   methods: {
-    obtenerEvento (filtro) {
-      this.datos_evento = []
-      filtro.get().then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          // Leemos los datos
-          const evento = {
-            foto: '',
-            nombre_evento: doc.data().nombre_evento,
-            localizacion: doc.data().localizacion,
-            precio: doc.data().precio,
-            fecha_inicio: doc.data().fecha_inicio,
-            votos: doc.data().votos,
-            comentarios: doc.data().comentarios,
-            usuario: doc.data().usuario,
-            isla: doc.data().isla,
-            id: doc.id,
-            foto_usuario: doc.data().foto_usuario
-          }
-
-          const storageRef = firebaseStg.ref('eventos/' + doc.id)
-          var fotoRef = storageRef.child('foto')
-          // Obtener foto
-          fotoRef.getDownloadURL().then(function (url) {
-            evento.foto = url
-          })
-
-          this.datos_evento.push(evento)
-        })
-      })
-    },
-    DataChild (tab, isla) {
-      this.tab = tab
-      this.isla = isla
-      this.Mostrar()
-    },
     Mostrar () {
       console.log('LEYENDO DOC')
-      // Consultas
-      var eventos = firebaseDb.collection('eventos')
-      var destacados = eventos.orderBy('votos', 'desc')
-      var novedades = eventos.orderBy('fecha_creacion', 'desc')
-      var zona = eventos.orderBy('fecha_creacion', 'desc')
-      // var isla = eventos.where('isla', '==', 'Tenerife')
 
-      // Gestión consultas
-      var consulta
-      if (this.tab === 'tab1') {
-        consulta = destacados
-      } else if (this.tab === 'tab2') {
-        consulta = novedades
-      } else if (this.tab === 'tab3') {
-        consulta = zona
-      }
-
-      // Requerimos filtro por isla si es necesario
-      if (this.isla !== '') {
-        consulta = consulta.where('isla', '==', this.isla)
-      }
-
-      // Realizamos consulta final
-      this.obtenerEvento(consulta)
+      axios({
+        method: 'put',
+        url: 'https://canarygo.herokuapp.com/eventos',
+        data: {
+          tipo: 'Consultar'
+        }
+      })
+        .then((response) => {
+          console.log('RESPUESTA DEL SERVER', response)
+          this.datos_evento = response.datos_evento
+        }, (error) => {
+          console.log('EL ERROR ES', error)
+        })
     }
   },
   mounted () {
